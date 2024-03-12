7945d0cb6eeb7d7560ae56370cb0990f0a8a3813
bichngoc94 <ngocbmt94@gmail.com>
2024-03-12 11:35:12 +0700

    refactoring table to compound component(re-use table)

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -8,18 +8,7 @@ import ConfirmDelete from "../../ui/ConfirmDelete.jsx";
 import { useDeleteCabin } from "./useDeleteCabin.js";
 import { useCreateCabin } from "./useCreateCabin.js";
 import Modal from "../../ui/Modal.jsx";
-
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
+import Table from "../../ui/Table.jsx";
 
 const Img = styled.img`
   display: block;
@@ -67,7 +56,7 @@ function CabinRow({ cabin }) {
 
   return (
     <>
-      <TableRow role="row">
+      <Table.Row role="row">
         <Img src={image} alt={name} />
         <Cabin>{name}</Cabin>
         <p>Fits up to {maxCapacity} guests</p>
@@ -108,7 +97,7 @@ function CabinRow({ cabin }) {
             <HiOutlineTrash />
           </ButtonIcon> */}
         </ButtonContainer>
-      </TableRow>
+      </Table.Row>
 
       {/* {show && <CreateCabinForm cabinToEdit={cabin} onCloseModal={() => setShow((s) => !s)} />} */}
     </>
